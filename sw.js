b44b499de5aaa59cd15b51cbc5f49b250c4470dd
sajjadprime786-sys <sajjadprime786@gmail.com>
2026-09-09 16:40:26 +0530

    Add service worker with notification handling

---
+++ sw.js
@@ -0,0 +1,14 @@
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    self.clients.matchAll({ type: 'window' }).then((clientsArr) => {
+      if (clientsArr.length > 0) {
+        return clientsArr[0].focus();
+      }
+      return self.clients.openWindow('./');
+    })
+  );
+});
